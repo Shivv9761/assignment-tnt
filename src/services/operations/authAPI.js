3a15db36b45b7b7e8eeb363f295b37bd02d72eb2
Shivam Varshney <94469594+Shivv9761@users.noreply.github.com>
2023-11-06 08:49:15 +0530

    Update authAPI.js

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -101,13 +101,9 @@ export function login(email, password, navigate) {
 
       toast.success("Login Successful")
       dispatch(setToken(response.data.token))
-      const userImage = response.data?.user?.image
-        ? response.data.user.image
-        : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.user.firstName} ${response.data.user.lastName}`
-      dispatch(setUser({ ...response.data.user, image: userImage }))
-      // const {user}=useSelector((state)=>state.profile)
+     
+      dispatch(setUser({ ...response.data.user}))
       console.log("my user =",response.data.user)
-      // console.log("my user =",user)
       // toast.success(user)
       localStorage.setItem("token", JSON.stringify(response.data.token))
       localStorage.setItem("user", JSON.stringify(response.data.user))

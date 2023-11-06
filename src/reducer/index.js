b1973226c457cef3ac0119f427823b11163a6cfb
Shivam Varshney <94469594+Shivv9761@users.noreply.github.com>
2023-11-06 08:33:07 +0530

    Update index.js

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,12 +2,10 @@ import {combineReducers} from "@reduxjs/toolkit";
 
 import authReducer from "../slices/authSlice"
 import profileReducer from "../slices/profileSlice";
-import courseReducer from "../slices/courseSlice"
 
 const rootReducer  = combineReducers({
     auth: authReducer,
     profile:profileReducer,
-    course:courseReducer,
 })
 
 export default rootReducer
